81fa93bfd0e8e889aaadb3e9a168bb8accfa8782
Helen <helen.romanycheva@gmail.com>
2022-05-07 22:21:50 +0300

    Добавьте файлы проекта.

---
+++ Assets/Abonents.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assets
+{
+    /// <summary>
+    /// Логика взаимодействия для Abobents.xaml
+    /// </summary>
+    public partial class Abobents : Page
+    {
+
+        
+
+        public Abobents()
+        {
+            InitializeComponent();
+            ComboType.ItemsSource = AssetsAccountingEntities.GetContext().Abonent.ToList();  
+
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                AssetsAccountingEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGAbonents.ItemsSource = AssetsAccountingEntities.GetContext().Abonent.ToList();
+             
+            }
+        }
+
+        private void ToursGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckActual_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckNoActual_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Assets/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assets
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Abobents());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void logotip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void abonents_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Abobents());
+        }
+
+        private void equipment_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Equipment());
+        }
+
+        private void assets_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Activ());
+        }
+
+        private void billing_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Billing());
+        }
+
+        private void support_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new SupportUsers());
+        }
+
+        private void crm_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new CRM());
+        }
+    }
+}
